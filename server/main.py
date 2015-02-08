b715362b2bce9c43fb0c288117a433155d415a92
tasti <tasti@zakarie.com>
2015-02-08 00:36:18 -0500

    Added start code for server

---
+++ server/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
